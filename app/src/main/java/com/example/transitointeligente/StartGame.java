f7a73018cbdb781fafb2edec98ab1cfe7a3abdb5
sabrainas <sazonass.m@gmail.com>
2024-08-31 16:04:11 -0300

    subindo app quiz transito inteligente

---
+++ app/src/main/java/com/example/transitointeligente/StartGame.java
@@ -0,0 +1,15 @@
+package com.example.transitointeligente;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_game);
+    }
+}
